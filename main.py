592bcd2edf578dd6e9ea3d4552e40704a18b312a
Häihälä <eero.haihala@gmail.com>
2015-03-20 09:03:52 +0200

    start of tkinter gui

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-__author__ = 'Häihälä'
+__author__ = 'Haihala'
 # this is the main file soon to be forgotten until the deadline is far too close.
+
+from Tkinter import *
+
+root = Tk()
+
+
+
+root.mainloop()
