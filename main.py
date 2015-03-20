e2451dbd0f83d2a6d3ba29c9d7ac095e3e5bd907
Häihälä <eero.haihala@gmail.com>
2015-03-20 09:14:31 +0200

    basic tkinter widgets

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ from Tkinter import *
 
 root = Tk()
 
+root.title("Humanitarian Aid Simulation Model")
+root.geometry("800x600")
+root.config(bg="grey5")
 
+label = Label(root, text="This is a label.")
+text = Text(root)
+button = Button(root, text="This is a button.")
+
+label.grid()
+text.grid()
+button.grid()
 
 root.mainloop()
