f248c5f53d7e0faa61486e25aba2302644fe1bfb
Häihälä <eero.haihala@gmail.com>
2015-03-20 16:03:01 +0200

    added a test slider and made the program terminate when killed via x on the top corner

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ import pygame
 import os
 
 
+def kill():
+    """the while loop at the end requires this to end python when the window is closed"""
+    # possible actions before termination
+    sys.exit(2)  # enter random exit code here
+
+
 def draw():
     """"This is a function for drawing the map where things move"""
     screen1 = pygame.display.set_mode((100, 100))
@@ -17,7 +23,7 @@ root = Tk()
 root.title("Humanitarian Aid Simulation Model")
 root.geometry("800x480")
 root.config(bg="grey40")
-#root.resizable(0, 0)
+# root.resizable(0, 0)
 
 # label = Label(root, text="This is a label.")
 # text = Text(root)
@@ -30,16 +36,15 @@ root.config(bg="grey40")
 mapFrame = Frame(root, width=650, height=320)
 mapFrame.grid(row=1, column=1)
 
-parameterFrame = Frame(root)  # , width=150, height=480)
+parameterFrame = Frame(root)
 parameterFrame.grid(row=1, column=2, rowspan=2)
-testParameters = Text(parameterFrame)
-testParameters.config(height=26, width=20)
-testParameters.grid(row=1)
+testParameters = Scale(parameterFrame, from_=0, to=200, orient=HORIZONTAL, label="test")
+testParameters.grid()
 runButton = Button(parameterFrame, text='Run you clever boy.')
 runButton.config(bg='red', height=3, font=("Times", 12, "bold"), activebackground='red')
 runButton.grid(sticky=N+S+E+W)
 
-infoFrame = Frame(root)  #, width=650, height=160)
+infoFrame = Frame(root)
 infoFrame.grid(row=2, column=1)
 testInfo = Text(infoFrame)
 testInfo.config(height=10)
@@ -53,6 +58,8 @@ pygame.display.init()
 pygame.display.update()
 
 draw()
+
+root.protocol('WM_DELETE_WINDOW', kill)  # because of the while loop it does not die when killed.
 
 root.mainloop()
 
